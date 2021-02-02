952165e387c97fb7b1a5c44fe6fd593ea637703b
Jin Igarashi <igarashi@mierune.co.jp>
2021-02-02 14:06:22 +0900

    updated download command

--- README.md
+++ README.md
@@ -10,6 +10,39 @@ npm i
 
 ## Usage
 
+### Target data
+
+`define.json`
 ```
-npm run download -- --output $(pwd)/data --definition $(pwd)/define.json
+[
+  ["110m", "cultural", "ne_110m_admin_0_countries_lakes"],
+  ["110m", "cultural", "ne_110m_admin_0_boundary_lines_land"],
+  ["110m", "physical", "ne_110m_coastline"],
+  ["110m", "physical", "ne_110m_ocean"],
+  ["110m", "physical", "ne_110m_land"]
+]
+```
+
+Please specify which data from Natural Earth you need.
+
+### Download Shapefile from Natural Earth
+
+```bash
+$ node src/index.js download -h
+Usage: index download [options] [output] [definition]
+
+It is a CLI tool which can download shapefile from Natural Earth
+
+Options:
+  -h, --help  display help for command
+```
+
+```bash
+npm run download -- $(pwd)/data $(pwd)/define.json
+```
+
+or
+
+```bash
+node src/index.js download $(pwd)/data $(pwd)/define.json
 ```
--- src/index.js
+++ src/index.js
@@ -2,28 +2,32 @@ const { program, Option } = require('commander');
 const path = require('path');
 const NeDownload = require('./download');
 
-const main = async() => {
-  program
-      .version('0.0.1')
-      .description('It is a CLI tool which can download shapefile from Natural Earth')
-      .addOption(new Option('-o, --output [value]', 'output directory path'))
-      .addOption(new Option('-d, --definition [value]', 'definition json file path'));
-  program.parse(process.argv);
-  const options = program.opts();
-  const outputDir = options.output
-  if (!outputDir){
+const download = async(output, definition) => {
+  if (!output){
     console.error("Please specify output directory path");
     return;
   }
-  const definition = options.definition;
   if (!definition){
     console.error("Please specify output definition JSON file path");
     return;
   }
   const define = require(definition)
-  const downloader = new NeDownload(outputDir, define);
+  const downloader = new NeDownload(output, define);
   const files = await downloader.download();
-  return files;
+  process.stdout.write(files.join(' '));
+}
+
+// see about commander.js
+// https://github.com/tj/commander.js/
+const main = async() => {
+  program
+    .version('0.0.1')
+    .command('download [output] [definition]')
+    .description('It is a CLI tool which can download shapefile from Natural Earth')
+    .action(async (output, definition, options) => {
+      await download(output, definition)
+    })
+  program.parse(process.argv);
 }
 
 module.exports = main();
